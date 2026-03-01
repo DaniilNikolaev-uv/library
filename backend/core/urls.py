48e2fb1f7661c363673ed8b298a1449dfda6fe8e
Nikolaev Daniil <lilwasd23@gmail.com>
2026-03-02 01:51:47 +0300

    add swagger

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from swagger.swagger import schema_view
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
